f18921f7fb69df7f36818ee5c8e9d6470a7cd11b
Sergio Bermejo de las Heras <sergio.bermejo123@gmail.com>
2023-04-25 22:18:59 +0200

    updated to lunarvim 1.3 (and nvim 0.9)

--- lvim/config.lua
+++ lvim/config.lua
@@ -1,65 +1,69 @@
---[[
-lvim is the global options object
-
-Linters should be
-filled in as strings with either
-a global executable or a path to
-an executable
-]]
--- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
-
--- general
+-- general `lvim` is the global options object
 lvim.log.level = "warn"
 lvim.format_on_save.enabled = false
 lvim.colorscheme = "lunar"
 
--- vim.wo.number = true
-vim.opt.number = true
+-- vim options
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
 vim.opt.relativenumber = true
 
--- keymappings
+-- general
+lvim.log.level = "info"
+
+--lvim.format_on_save = {
+--  enabled = true,
+--  pattern = "*.lua",
+--  timeout = 1000,
+--}
+
+-- keymappings <https://www.lunarvim.org/docs/configuration/keybindings>
 lvim.leader = "space"
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
--- lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
--- lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+
+lvim.colorscheme = "lunar"
 
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.terminal.active = true
+
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 
--- if you don't want all the parsers change this to a table of the ones you want
-lvim.builtin.treesitter.ensure_installed = {
-  "bash", "c", "javascript", "json", "lua",
-  "python", "typescript", "tsx", "css",
-  "rust", "java", "yaml",
-}
-
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enable = true
+lvim.builtin.treesitter.ensure_installed = {
+    "bash", "c", "javascript", "json", "lua",
+    "python", "typescript", "tsx", "css",
+    "rust", "java", "yaml",
+}
+-- Automatically install missing parsers when entering buffer
+lvim.builtin.treesitter.auto_install = true
 
 lvim.plugins = {
-  { "unblevable/quick-scope" }, {
-  "nacro90/numb.nvim",
-  event = "BufRead",
-  config = function()
-    require("numb").setup {
-      show_numbers = true,         -- Enable 'number' for the window while peeking
-      show_cursorline = true,      -- Enable 'cursorline' for the window while peeking
-      hide_relativenumbers = true, -- Enable turning off 'relativenumber' for the window while peeking
-      number_only = false,         -- Peek only when the command is only a number instead of when it starts with a number
-      centered_peeking = true,     -- Peeked line will be centered relative to window
-    }
-  end,
-}, {
-  "folke/todo-comments.nvim",
-  event = "BufRead",
-  config = function()
-    require("todo-comments").setup()
-  end,
-},
+    { "unblevable/quick-scope" },
+    {
+        "nacro90/numb.nvim",
+        event = "BufRead",
+        config = function()
+            require("numb").setup {
+                show_numbers = true,         -- Enable 'number' for the window while peeking
+                show_cursorline = true,      -- Enable 'cursorline' for the window while peeking
+                hide_relativenumbers = true, -- Enable turning off 'relativenumber' for the window while peeking
+                number_only = false,         -- Peek only when the command is only a number instead of when it starts with a number
+                centered_peeking = true,     -- Peeked line will be centered relative to window
+            }
+        end,
+    },
+    {
+        "folke/todo-comments.nvim",
+        event = "BufRead",
+        config = function()
+            require("todo-comments").setup()
+        end,
+    },
 }
+
 vim.cmd [[
 let g:qs_highlight_on_keys = ['f', 'F', 't', 'T']
 ]]
